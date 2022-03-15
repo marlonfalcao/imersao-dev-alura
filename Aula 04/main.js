95070d1270079f57376f4668dcfae199c18f6b3a
Marlon Santos <marlon.falcao@icloud.com>
2022-03-15 08:05:24 -0300

    Adição da aula 4, estudando  o addEventListener e DOM, adicao de film ok, pendente remocao

--- Aula 04/main.js
+++ Aula 04/main.js
@@ -1,36 +1,44 @@
 //https://www.omelete.com.br/imagens/cinema/artigos/dia_depois_amanha/poster.jpg
 
-const addFilmes = document.querySelector("form")
-const inputs = addFilmes.querySelectorAll('input');
-
+const addFilmes = document.querySelector("#addFilme");
+const inputs = addFilmes.querySelectorAll("input");
 
 
 addFilmes.addEventListener("submit", event => {
     event.preventDefault()
     
-    //let imageFilme = ""
-
     inputs.forEach((input) => {
         imageFilme = (String(input.value))
         input.value = ""
         
-        let pai = document.createElement("img");
-        pai.src = imageFilme;
-        
-        document.getElementById("listaImagens").appendChild(pai)
+        if (imageFilme.endsWith(".jpg")){
 
+            let filho = document.createElement("img");
+            filho.src = imageFilme;
+            
+            document.getElementById("listaImagens").appendChild(filho)
+        } else {
+            alert("Imagem inválida!")
+        }
+    
     })
 
 })
 
-//let imagem = "https://www.omelete.com.br/imagens/cinema/artigos/dia_depois_amanha/poster.jpg";
-
-//let mostraImagem = document.createElement("img");
-
-//listaTeste = document.getElementById("listaImagens").appendChild()
-
-//console.log()
+const removeFilmes = document.querySelector("#removerFilme");
+const inputsRemover = removeFilmes.querySelectorAll("input");
 
 
+
+removeFilmes.addEventListener("submit", event => {
+    event.preventDefault()
+
+    inputsRemover.forEach((input) => {
+        imageFilme = (String(input.value))
+        input.value = ""
+
+    })
+    
+})
 
 
